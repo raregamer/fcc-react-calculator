5ccb264aa0c24f32c5311e0b96b6bd70de790951
Samuel <raregamer@hotmail.com>
2019-09-13 09:07:06 -0700

    Finshed logic

--- src/Display.js
+++ src/Display.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 class Display extends React.Component{
+    
     render(){
         return(
             <div className = "display-container">
-            <h1 id = "display" >{this.props.currentValue} </h1>
-            <h2> {this.props.userTokenStack}</h2>
+            <div className="display-split">
+            <h3 id = "current-equation">{this.props.currentValue}</h3>
+            <h2 id="display">{this.props.currentInput}</h2>
+            </div>
             </div>
         )
     }
